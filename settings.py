dcf2e04dac3f2f9ccbdaa4b7c077a4e895c331a6
Gwyn Bleidd <berenluthien1977@gmail.com>
2020-06-19 17:30:10 +0400

    adding support for existing variable to be configurable from container ENV; adding k8s manifests for single node redis

--- settings.py
+++ settings.py
@@ -1,10 +1,10 @@
 import os
 
 # Server settings
-SERVER_HOST = "0.0.0.0"
+SERVER_HOST = os.environ.get("SERVER_HOST", "0.0.0.0")
 SERVER_PORT = os.environ.get("SERVER_PORT", 8080)
 
 # Redis settings
 REDIS_HOST = os.environ.get("REDIS_HOST", "localhost")
-REDIS_PORT = 6379
-REDIS_PWD = ""
+REDIS_PORT = os.environ.get("REDIS_PORT", 6379)
+REDIS_PWD = os.environ.get("REDIS_PWD", "")
